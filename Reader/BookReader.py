59efd110e8d4e5c518f3566e6f179c37be3f7227
Lighttx26 <ncnhatquang26@gmail.com>
2024-04-09 18:39:35 +0700

    refactor: use reader class for reading pages, use config/config.yml to save config such as path, kernel size, ...

---
+++ Reader/BookReader.py
@@ -0,0 +1,95 @@
+import numpy as np
+from PIL import Image
+from vietocr.tool.predictor import Predictor
+import cv2
+
+from img_processing import img_processing as imgp, extractor
+from model.utils import get_config
+
+
+class BookReader:
+    def __init__(self, config=None):
+        if config is None:
+            config = {
+                'mode': 'word',
+                'page': 'double',
+                'line_ksize': (12, 3),
+                'word_ksize': (8, 10)
+            }
+
+        self.config = config
+
+        model_config = get_config.load_config_from_file("model\\config\\base.yml", "model\\config\\vgg-seq2seq.yml")
+        model_config['device'] = 'cpu'
+        model_config['weights'] = 'model/config/vgg_seq2seq.pth'
+        self.detector = Predictor(model_config)
+
+    def set_config(self, config):
+        self.config = config
+
+    def read(self, image):
+        if self.config is None:
+            raise ValueError("Config is not set.")
+
+        if self.config['page'] == 'double':
+            resized_image = imgp.resize_image(image, new_width=2000)
+            pages = extractor.separate_pages(resized_image)
+
+            return self.__read_page(pages[0]) + self.__read_page(pages[1])
+
+        elif self.config['page'] == 'single':
+            resized_image = imgp.resize_image(image, new_width=1000)
+            return self.__read_page(resized_image)
+
+        else:
+            raise ValueError("This page mode isn't supported. Choose 'single' or 'double'.")
+
+    def __read_page(self, page):
+        """
+        Read text from a page
+        :param page: image of a page
+        :return: text from the page
+        """
+        lines_bound = extractor.detect_lines(page, ksize=self.config['line_ksize'], show_result=False)
+        lines_mask = extractor.extract_lines_mask(page, ksize=self.config['line_ksize'])
+
+        left_offset = np.median([line[0][0] for line in lines_bound[1] if line[0][2] > 500])
+        right_offset = np.median([line[-1][0] + line[-1][2] for line in lines_bound[1] if line[-1][2] > 500])
+
+        result = ""
+
+        for i, line in enumerate(lines_bound[0]):
+            for j, subline in enumerate(line):
+                if (lines_bound[1][i][j][0] < left_offset - 100) or (
+                        lines_bound[1][i][j][0] + lines_bound[1][i][j][2] > right_offset + 100):
+                    continue
+
+                result += self.__read_line(subline, lines_mask[i][j]) + " "
+            result += "\n"
+
+        return result
+
+    def __read_line(self, line, line_mask):
+        """
+        Read text from a line
+        :param line: image of a line
+        :return: text from the line
+        """
+        if self.config['mode'] == 'line':
+            return self.detector.predict(Image.fromarray(line))
+
+        elif self.config['mode'] == 'word':
+            result = ""
+            words = extractor.detect_words_in_line(line, line_mask, ksize=self.config['word_ksize'], show_result=False)[0]
+            for word in words:
+                word_img = Image.fromarray(word)
+                s = self.detector.predict(word_img)
+                if len(s) == 1:
+                    if not imgp.is_char_ratio(char_shape=word.shape, line_shape=line.shape):
+                        s = '-'
+                result += s + " "
+
+            return result
+
+        else:
+            raise ValueError("This mode isn't supported. Choose 'line', 'line-word' or 'word'.")
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from model import predictor
+from Reader.BookReader import BookReader
 import os
 import cv2
 import yaml
 
 if __name__ == "__main__":
 
-    with open("file.yaml", "r") as f:
-        config = yaml.load(f)
+    with open("config/config.yml", "r") as f:
+        config = yaml.safe_load(f)
 
     input_path = config['input_path']
     output_path = config['output_path']
 
-    image_path = os.path.join(input_path, 'image.jpg')
+    image_path = os.path.join(input_path, 'test.jpeg')
     image = cv2.imread(image_path)
 
     if image is None:
         print("Failed to load image.")
     else:
-        # mode: line, line-word or word
-        # page: single or double
-        predictor.predict(image, mode='word', page='double', save_result=(True, output_path))
+        reader_config = config['reader']
+        reader = BookReader(reader_config)
+        print(reader.read(image))
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
